f8c1faa8dcdaf3e58353d5845cc6ea59a59eee6c
Tory Dannenberg <rfdberg@gmail.com>
2022-04-06 15:57:52 -0600

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const groceriesRoutes = require('./routes/groceries.routes');
-const authRoutes = require('./routes/auth.routes');
-const userRoutes = require('./routes/user.routes');
+const groceriesRoutes = require("./routes/groceries.routes");
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const logLevel = process.env.LOG_LEVEL || 'dev';
+const logLevel = process.env.LOG_LEVEL || "dev";
 
 app.use(logger(logLevel));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/groceries', groceriesRoutes);
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/groceries", groceriesRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}!`);
-})
+});
